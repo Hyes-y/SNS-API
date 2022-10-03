17aa0feb633068a5d6894342645c9642a4380d05
Hyes-y <gptjs7970@naver.com>
2022-10-03 22:14:37 +0900

    feat: 태그 필터링, 페이지네이션 기능 구현 - 요구사항에 맞는 태그 필터링 기능 구현 - 요구사항에 맞는 페이지네이션 기능 구현

--- apps/post/views.py
+++ apps/post/views.py
@@ -3,37 +3,71 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .permissions import IsOwnerOrReadOnly
+from .paginations import CustomPageNumberPagination
 from .serializers import PostSerializer
-from .models import Post, HashTag
+from .models import Post
+from django.db.models import Count
 
 
 class PostViewSet(ModelViewSet):
+    """ 게시글 CRUD API """
     queryset = Post.objects.all().prefetch_related('hashtags')
     serializer_class = PostSerializer
+    pagination_class = CustomPageNumberPagination
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title']
+    ordering_fields = ['created_at', 'likes', 'views']
+    ordering = ['-created_at']
 
     def get_permissions(self):
-        if self.action in ('list', 'retrieve', 'create'):
-            permission_classes = [IsAuthenticated]
-        else:
+        """ permission 반환 함수 : action별 권한 설정 """
+        if self.action in ('update', 'destroy', 'recovery'):
             permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+        else:
+            permission_classes = [IsAuthenticated]
+
         return [permission() for permission in permission_classes]
 
     def get_queryset(self):
+        """ queryset 반환 함수 : 태그 필터링 포함 """
         if self.action in ('list', 'retrieve', 'create'):
-            queryset = Post.objects.filter(is_deleted=False).prefetch_related('hashtags')
+            params = self.request.query_params.get('tags', None)
+            if params:
+                # 태그 필터링
+                params = params.split(",")
+                queryset = \
+                    Post.objects\
+                        .filter(is_deleted=False, hashtags__content__in=params)\
+                        .annotate(tag_cnt=Count('id'))\
+                        .filter(tag_cnt=len(params))
+            else:
+                queryset = Post.objects.filter(is_deleted=False).prefetch_related('hashtags')
+
         elif self.action in ('update', 'destroy'):
             queryset = Post.objects.filter(user=self.request.user, is_deleted=False).prefetch_related('hashtags')
+
         else:
+            # 삭제된 게시글 복구하는 경우
             queryset = Post.objects.filter(user=self.request.user).prefetch_related('hashtags')
+
         return queryset
 
     def perform_destroy(self, instance):
+        """
+        게시글 삭제
+        is_deleted: True 로 변경
+        """
         # instance.hashtags.clear()
         instance.is_deleted = True
         instance.save()
 
     def retrieve(self, request, *args, **kwargs):
+        """
+        게시글 조회
+        조회시 hit()를 통해 조회수 증가
+        """
         instance = self.get_object()
         serializer = self.get_serializer(instance)
         instance.hit()
@@ -41,6 +75,10 @@ class PostViewSet(ModelViewSet):
 
     @action(detail=True, methods=['get'])
     def recovery(self, request, **kwargs):
+        """
+        삭제된 게시글 복구 action
+        is_deleted: False 로 변경
+        """
         instance = self.get_object()
         if instance.is_deleted:
             instance.is_deleted = False
@@ -53,6 +91,11 @@ class PostViewSet(ModelViewSet):
 
     @action(detail=True, methods=['put'])
     def like(self, request, **kwargs):
+        """
+        좋아요 기능
+        좋아요 요청시 이미 좋아요 데이터가 있는 경우 좋아요 취소
+        없는 경우 데이터 추가
+        """
         instance = self.get_object()
         if self.request.user in instance.likes.all():
             instance.likes.remove(self.request.user)
